c956b8f9261d33d8c9dcbca85d02eb6337f4a3b3
pie <yejinh_h@naver.com>
2025-01-08 14:48:05 +0900

    feat : coupon entity 생성

--- coupon-api/src/main/java/com/project/couponapi/CouponApiApplication.java
+++ coupon-api/src/main/java/com/project/couponapi/CouponApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import com.project.couponcore.CouponCoreConfiguration;
+import com.project.couponcore.config.CouponCoreConfiguration;
 
 @Import(CouponCoreConfiguration.class)
 @SpringBootApplication
